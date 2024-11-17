406162d77212aca039245e373e25d7c910501427
Alexander <sany199574@gmail.com>
2024-11-17 23:46:52 +0300

    Добавлен Web проект

---
+++ src/PS.BioBoard/PS.BioBoard.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PS.BioBoard.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
