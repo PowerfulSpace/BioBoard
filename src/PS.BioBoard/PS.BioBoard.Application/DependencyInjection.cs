dddeecb151ac1a719eaf68c1e7c31de5390b44f6
Alexander <sany199574@gmail.com>
2024-11-19 22:13:55 +0300

    Исправлены ошибки с использованием строк подключения. Изменены представления

--- src/PS.BioBoard/PS.BioBoard.Application/DependencyInjection.cs
+++ src/PS.BioBoard/PS.BioBoard.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using PS.BioBoard.Application.Services.Persons;
 
 namespace PS.BioBoard.Application
 {
@@ -6,6 +7,7 @@ namespace PS.BioBoard.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddScoped<IPersonService, PersonService>();
             return services;
         }
     }
