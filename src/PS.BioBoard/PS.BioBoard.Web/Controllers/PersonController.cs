dd62420eedbbc4643ed10b8407fee19eb0ac9b66
Alexander <sany199574@gmail.com>
2024-11-19 18:52:36 +0300

    Добавлено представление для Index

--- src/PS.BioBoard/PS.BioBoard.Web/Controllers/PersonController.cs
+++ src/PS.BioBoard/PS.BioBoard.Web/Controllers/PersonController.cs
@@ -1,12 +1,94 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PS.BioBoard.Application.Services.Persons;
+using PS.BioBoard.Domain.Entites;
 
 namespace PS.BioBoard.Web.Controllers
 {
     public class PersonController : Controller
     {
-        public IActionResult Index()
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var persons = await _personService.GetAllAsync();
+            return View(persons);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var person = await _personService.GetByIdAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                await _personService.AddAsync(person);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var person = await _personService.GetByIdAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _personService.UpdateAsync(person);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var person = await _personService.GetByIdAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            await _personService.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
