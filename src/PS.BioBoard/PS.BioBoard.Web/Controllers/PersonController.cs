ec16b0118af3982283183ddb9ec826f7656b8948
Alexander <sany199574@gmail.com>
2024-11-19 03:10:45 +0300

    Приведён код в порядок

---
+++ src/PS.BioBoard/PS.BioBoard.Web/Controllers/PersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS.BioBoard.Web.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
