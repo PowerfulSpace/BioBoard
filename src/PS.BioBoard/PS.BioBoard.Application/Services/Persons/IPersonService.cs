fe401017d07e645ac795dd2aa141d8c8ee6da32e
Alexander <sany199574@gmail.com>
2024-11-19 18:43:31 +0300

    Добавление сервиса Person

---
+++ src/PS.BioBoard/PS.BioBoard.Application/Services/Persons/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using PS.BioBoard.Domain.Entites;
+
+namespace PS.BioBoard.Application.Services.Persons
+{
+    public interface IPersonService
+    {
+        Task<IEnumerable<Person>> GetAllAsync();
+        Task<Person> GetByIdAsync(Guid id);
+        Task AddAsync(Person person);
+        Task UpdateAsync(Person person);
+        Task DeleteAsync(Guid id);
+    }
+}
