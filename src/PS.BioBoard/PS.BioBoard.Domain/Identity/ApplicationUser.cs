e7005ab10605d72c4f1ab6a9859bdcb7b92b24e9
Alexander <sany199574@gmail.com>
2024-11-20 19:59:50 +0300

    Добавлен объект для аутентификации

---
+++ src/PS.BioBoard/PS.BioBoard.Domain/Identity/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿namespace PS.BioBoard.Domain.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Дополнительные свойства пользователя
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+
+
+        // Свойство для отображения полного имени
+        public string FullName => $"{FirstName} {LastName}";
+
+
+        // Дополнительные метаданные пользователя
+        public string ProfilePictureUrl { get; set; } = null!;
+        public string Bio { get; set; } = null!;
+    }
+}
